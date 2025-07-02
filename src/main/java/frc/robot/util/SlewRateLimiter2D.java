aac279f376448ffd42101fc8013bd14acbfcfbc4
Trevor Sears <trevor@trevorsears.com>
2025-07-02 18:22:26 -0400

    Added new `SlewRateLimiter2D` class.

---
+++ src/main/java/frc/robot/util/SlewRateLimiter2D.java
@@ -0,0 +1,65 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+
+public abstract class SlewRateLimiter2D<T> {
+
+    /**
+     * The maximum rate of change allowed in the overall magnitude of the
+     * Translation2d per second.
+     */
+    protected final double rateLimit;
+
+    /**
+     * The last value that was returned by the calculate method.
+     */
+    protected Translation2d lastValue;
+
+    /**
+     * The last time the calculate method was called.
+     */
+    protected double lastTime;
+
+    /**
+     * Initializes a new SlewRateLimiter2D with the specified rate limit (in units per second).
+     *
+     * @param rateLimit The maximum rate of change allowed in the overall magnitude of the
+     * Translation2d per second.
+     */
+    public SlewRateLimiter2D(double rateLimit) {
+
+        this.rateLimit = rateLimit;
+        this.lastValue = new Translation2d(0, 0);
+        this.lastTime = Timer.getFPGATimestamp();
+
+    }
+
+    protected abstract Translation2d toTranslation2d(T value);
+
+    protected abstract T fromTranslation2d(Translation2d value);
+
+    public T calculate(T value) {
+
+        double currentTime = Timer.getFPGATimestamp();
+        double deltaTime = currentTime - lastTime;
+
+        if (deltaTime <= 0) return value;
+
+        Translation2d currentValue = this.toTranslation2d(value);
+        Translation2d delta = currentValue.minus(lastValue);
+
+        double maximumDelta = this.rateLimit * deltaTime;
+        double requestedDelta = delta.getNorm();
+        double scalingFactor = Math.min(maximumDelta/requestedDelta, 1);
+
+        Translation2d resultantValue = lastValue.plus(delta.times(scalingFactor));
+
+        lastValue = resultantValue;
+        lastTime = currentTime;
+
+        return this.fromTranslation2d(lastValue);
+
+    }
+
+}
