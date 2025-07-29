147ee2fb8fd2bfab47dad9e6a21dc945312942cc
Trevor Sears <trevor@trevorsears.com>
2025-07-29 19:18:25 -0400

    Updated `ChassisSpeedsSlewRateLimiter` to allow for asymmetric rate limits.

--- src/main/java/frc/robot/util/ChassisSpeedsSlewRateLimiter.java
+++ src/main/java/frc/robot/util/ChassisSpeedsSlewRateLimiter.java
@@ -1,6 +1,5 @@
 package frc.robot.util;
 
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.measure.AngularAcceleration;
@@ -10,31 +9,55 @@ import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 
 public class ChassisSpeedsSlewRateLimiter {
-
+    
     protected final SlewRateLimiterTranslation2D translationSlewRatLimiter;
-
-    protected final SlewRateLimiter angularSlewRateLimiter;
-
+    
+    protected final ZeroCenteredSlewRateLimiter angularSlewRateLimiter;
+    
     public ChassisSpeedsSlewRateLimiter(
         LinearAcceleration maxLinearAcceleration,
         AngularAcceleration maxAngularAcceleration
     ) {
 
-        this.translationSlewRatLimiter =
-            new SlewRateLimiterTranslation2D(maxLinearAcceleration.in(MetersPerSecondPerSecond));
-        this.angularSlewRateLimiter =
-            new SlewRateLimiter(maxAngularAcceleration.in(RadiansPerSecondPerSecond));
+        this(
+            maxLinearAcceleration,
+            maxLinearAcceleration,
+            maxAngularAcceleration,
+            maxAngularAcceleration
+        );
+
+    }
+
+    public ChassisSpeedsSlewRateLimiter(
+        LinearAcceleration maxLinearAcceleration,
+        LinearAcceleration maxLinearDeceleration,
+        AngularAcceleration maxAngularAcceleration,
+        AngularAcceleration maxAngularDeceleration
+    ) {
+
+        this.translationSlewRatLimiter = new SlewRateLimiterTranslation2D(
+            maxLinearAcceleration.in(MetersPerSecondPerSecond),
+            maxLinearDeceleration.in(MetersPerSecondPerSecond)
+        );
+        this.angularSlewRateLimiter = new ZeroCenteredSlewRateLimiter(
+            maxAngularAcceleration.in(RadiansPerSecondPerSecond),
+            maxAngularDeceleration.in(RadiansPerSecondPerSecond),
+            0
+        );
 
     }
 
     public ChassisSpeeds calculate(ChassisSpeeds value) {
 
-        Translation2d translation = this.translationSlewRatLimiter.calculate(new Translation2d(
-            value.vxMetersPerSecond,
-            value.vyMetersPerSecond
-        ));
+        Translation2d translation = this.translationSlewRatLimiter.calculate(
+            new Translation2d(
+                value.vxMetersPerSecond,
+                value.vyMetersPerSecond
+            )
+        );
 
-        double omegaRadiansPerSecond = this.angularSlewRateLimiter.calculate(value.omegaRadiansPerSecond);
+        double omegaRadiansPerSecond = this.angularSlewRateLimiter
+            .calculate(value.omegaRadiansPerSecond);
 
         return new ChassisSpeeds(
             translation.getX(),
