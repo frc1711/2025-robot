9c11a41f8dcfbdef5d555d0b6f87d285357216af
Trevor Sears <trevor@trevorsears.com>
2025-08-05 20:00:59 -0400

    Overhauled/cleaned-up `raiseAndShoot` and `autoScoreOnReef`.

--- src/main/java/frc/robot/ComplexCommands.java
+++ src/main/java/frc/robot/ComplexCommands.java
@@ -71,25 +71,27 @@ public class ComplexCommands {
 	
 	public Command raiseAndShoot(ReefLevel level) {
 
-		Command moveElevatorToPosition = this.robot.elevator.commands.goTo(level.elevatorScoringPosition);
-		Command waitForElevatorToMoveToPosition = this.robot.elevator.commands.waitUntilAtPosition(level.elevatorScoringPosition);
+		Command positionElevator = this.robot.elevator.commands.goTo(level.elevatorScoringPosition);
 		Command shoot = this.robot.mailbox.commands.feed().withTimeout(Seconds.of(0.5));
-		Command raiseAndShoot = this.drive().alongWith(
-			moveElevatorToPosition.withDeadline(waitForElevatorToMoveToPosition.andThen(shoot))
-		);
-		Runnable resetLights = () -> this.robot.lights.set(StatusLightsPattern.SOLID_COLORS_WHITE);
+		Command raiseAndShoot = positionElevator.andThen(shoot);
+		Runnable resetElevatorAndLights = () -> {
+			this.robot.elevator.goToPosition(ElevatorPosition.RESTING);
+			this.robot.lights.set(StatusLightsPattern.SOLID_COLORS_WHITE);
+		};
 		
 		if (level.equals(ReefLevel.L4)) {
 			
 			raiseAndShoot = raiseAndShoot.andThen(
-				this.robot.elevator.commands.goTo(ElevatorPosition.L4_TIP)
-					.alongWith(this.robot.mailbox.commands.feed())
-					.until(this.robot.elevator.triggers.isAtPosition(ElevatorPosition.L4_TIP))
+				this.robot.mailbox.commands.feed()
+					.withDeadline(this.robot.elevator.commands.goTo(ElevatorPosition.L4_TIP))
 			);
 			
 		}
 		
-		return raiseAndShoot.finallyDo(resetLights);
+		return this.drive()
+			.alongWith(raiseAndShoot)
+			.andThen(this.robot.elevator.commands.goTo(ElevatorPosition.RESTING))
+			.finallyDo(resetElevatorAndLights);
 		
 	}
 	
@@ -116,60 +118,38 @@ public class ComplexCommands {
 	
 	public Command autoScoreOnReef(IntSupplier reefTagID, ReefLevel level, ReefAlignment branch) {
 		
-		ElevatorPosition elevatorPosition = switch (level) {
-			case L1_TROUGH -> ElevatorPosition.L1_SCORING;
-			case L2 -> ElevatorPosition.L2_SCORING;
-			case L3 -> ElevatorPosition.L3_SCORING;
-			case L4 -> ElevatorPosition.L4_SCORING;
-		};
-		Distance extra = level.equals(ReefLevel.L4)
-				? Inches.of(-2)
-				: Inches.of(0);
-		Command startLog = new LogCommand(
-			"Attempting to auto-score on reef. [April tag ID: " +
-			reefTagID.getAsInt() + ", level: " + level + ", branch: " + branch +
-			"]"
-		);
-		Supplier<Pose2d> poseSupplier = () ->
-			RobotPoseBuilder.getReefScoringPose(reefTagID.getAsInt(), branch)
-				.withRobotRelativeTranslation(new Translation2d(extra, Inches.of(0)))
-				.get();
+		Distance extra = Inches.of(level.equals(ReefLevel.L4) ? -2 : 0);
+		Supplier<Pose2d> poseSupplier = RobotPoseBuilder.getReefScoringPose(reefTagID, branch)
+			.withRobotRelativeTranslation(new Translation2d(extra, Inches.of(0)));
 		Command goToScoringPosition = this.robot.swerve.commands.goToPosition(
 			poseSupplier,
-			InchesPerSecond.of(40),
 			Inches.of(0.25),
 			Degrees.of(1),
 			() -> new int[] { reefTagID.getAsInt() }
 		);
-		Command waitUntilPositionedToScore = this.robot.swerve.commands
-			.waitUntilAtPosition(poseSupplier, Inches.of(0.375), Degrees.of(1))
-			.alongWith(Commands.waitUntil(this.robot.elevator.triggers.isAtPosition(elevatorPosition)));
-		Command raiseElevator = new InstantCommand(() -> this.robot.elevator.goToPosition(elevatorPosition));
 		Command scoreCoral = this.robot.mailbox.commands.feed(level).withTimeout(0.6);
 		Command positionCoral = robot.complexCommands.autoAcceptMail()
 			.alongWith(robot.complexCommands.autofeedMailbox())
 			.until(robot.intake.triggers.isCoralInUpperIntake().negate().and(robot.intake.triggers.isCoralInLowerIntake().negate()));
+		Command prepareToScore = autoAcceptMail().withDeadline(
+			goToScoringPosition
+				.alongWith(this.robot.elevator.commands.goTo(level.elevatorScoringPosition))
+		);
 
-		if (level == ReefLevel.L4) {
-			
+		if (level.equals(ReefLevel.L4)) {
+
 			scoreCoral = scoreCoral.andThen(
-				this.robot.elevator.commands.goTo(ElevatorPosition.L4_TIP)
-					.alongWith(this.robot.mailbox.commands.feed())
-					.until(this.robot.elevator.triggers.isAtPosition(ElevatorPosition.L4_TIP))
+				this.robot.mailbox.commands.feed()
+					.withDeadline(this.robot.elevator.commands.goTo(ElevatorPosition.L4_TIP))
 			);
-			
+
 		}
 		
-		return startLog.andThen(
-			positionCoral
-		.andThen(new WaitCommand(0.25))
-//		.andThen(goToCalibrationPosition)
-		.andThen(goToScoringPosition
-			.alongWith(raiseElevator)
-			.alongWith(autoAcceptMail())
-			.withDeadline(waitUntilPositionedToScore)
+		return positionCoral
+			.andThen(prepareToScore)
+			.andThen(new WaitCommand(Seconds.of(0.25)))
 			.andThen(scoreCoral)
-		)).finallyDo(() -> this.robot.elevator.goToPosition(ElevatorPosition.RESTING));
+			.finallyDo(() -> this.robot.elevator.goToPosition(ElevatorPosition.RESTING));
 		
 	}
 	
